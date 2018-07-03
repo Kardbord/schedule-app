932f1de6450130881bf53fd31c1c41689745b55f
Kardbord <tanner.kvarfordt@gmail.com>
2018-07-03 08:54:52 -0600

    Dynamically populated course library

--- src/components/library/libraryCourse.js
+++ src/components/library/libraryCourse.js
@@ -10,7 +10,7 @@ class LibraryCourse extends Component {
             <div className="library-course">
                 <div className="library-course__title-check">
                     <label className="library-course__title">
-                        Problem Solving
+                        {this.props.title}
                     </label>
                     {Icon("fas fa-check", "library-course__icon")}
                 </div>
@@ -18,17 +18,7 @@ class LibraryCourse extends Component {
                 <Action className="library-course__action" />
                 <div className="library-course__description">
                     <label>Course Description</label>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Vivamus quis placerat ante, et commodo ipsum. Aliquam
-                        nec ante non est aliquet interdum. Pellentesque
-                        vestibulum ipsum a pulvinar cursus. Integer congue arcu
-                        nec odio convallis, et iaculis massa congue. Sed
-                        efficitur elit erat, eu facilisis enim sodales quis.
-                        Suspendisse varius lacus nisl, et malesuada ligula
-                        feugiat ornare. Suspendisse tristique mattis nibh, nec
-                        rutrum odio volutpat ac.
-                    </p>
+                    <p>{this.props.description}</p>
                 </div>
             </div>
         );
