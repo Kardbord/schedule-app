403a16075c9bf9d867cb7f12d033da7ebfd6bb0d
Kardbord <tanner.kvarfordt@aggiemail.usu.edu>
2018-07-05 13:55:11 -0600

    styled progress tracker

--- src/components/schedule/progressTracker.js
+++ src/components/schedule/progressTracker.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 
 class ProgressTracker extends Component {
 
-    calculateProgress = function() {
+    calculateProgress = function () {
         return (this.props.amountEnrolled / this.props.amountPossible) * 100;
 
     }.bind(this);
@@ -10,11 +10,11 @@ class ProgressTracker extends Component {
     render() {
         return (
             <div className="progress-tracker">
-                <label className="progress-tracker__label">
+                <div className="progress-tracker__title">
                     Progress Tracker
-                </label>
+                </div>
                 <div className="progress-tracker__percentage">
-                    {this.calculateProgress()}
+                    {this.calculateProgress()}%
                 </div>
             </div>
         );
