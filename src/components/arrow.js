15596dfeb503c32ad755e8db51aa9f1892e52f67
Kardbord <tanner.kvarfordt@gmail.com>
2018-07-05 08:56:45 -0600

    Added arrow flip animation

--- src/components/arrow.js
+++ src/components/arrow.js
@@ -1,11 +1,26 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Arrow extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            status: false
+        };
+    }
+
+    toggleArrow = function(props) {
+        document.getElementById('arrow').classList.toggle('arrow-closed');
+        this.setState({ status: !this.state.status });
+    }.bind(this);
+
     render() {
         return (
-            <div className={`${this.props.className} arrow`}>
-                
-            </div>
+            <a
+                onClick={() => this.toggleArrow()}
+                id="arrow"
+                className={`${this.props.className} arrow`}
+            />
         );
     }
 }
